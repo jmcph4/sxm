00bbb43bedc0155245a1f06f767f4205e3121ce1
Jack McPherson <jmcph4@users.noreply.github.com>
2017-02-03 12:20:11 +1000

    Modify `sxm_List` to be handle class
    This commit modifies `sxm_List` (in `sxm_List.m`) to be a handle class
    rather than a vlaue class (the default MATLAB behaviour). This now
    means that `sxm_List` objects store internal state.

--- sxm_List.m
+++ sxm_List.m
@@ -1,4 +1,4 @@
-classdef sxm_List
+classdef sxm_List < handle
     %sxm_List Implements the linear list ADT
     
     properties (SetAccess=protected)
@@ -17,54 +17,60 @@ classdef sxm_List
         end
         
         function s = size(L)
-            s = L.len;
+            s = [1 L.len];
         end
         
-        function sref = subsref(L, s)
-            switch s(1).type
-                case '.'
-                    sref = builtin('subsref', L, s);
-                case '()'
-                    if length(s) < 2
-                        sref = builtin('subsref', L.data, s);
-                        return
-                    else
-                        error('sxm_List:subsref', ...
-                            'Not a supported subscripted reference')
-                    end
-                case '{}'
-                    error('sxm_List:subsref', ...
-                        'Not a supported subscripted reference')
-            end
+%         function sref = subsref(L, s)
+%             switch s(1).type
+%                 case '.'
+%                     sref = builtin('subsref', L, s);
+%                 case '()'
+%                     if length(s) < 2
+%                         sref = builtin('subsref', L.data, s);
+%                         return
+%                     else
+%                         error('sxm_List:subsref', ...
+%                             'Not a supported subscripted reference')
+%                     end
+%                 case '{}'
+%                     error('sxm_List:subsref', ...
+%                         'Not a supported subscripted reference')
+%             end
+%         end
+        
+        function append(L, e)
+            L.insert(e, L.len);
         end
         
-        function Lnew = append(L, e)
-            Lnew = sxm_List([L.data e]);
-        end
-        
-        function Lnew = insert(L, e, pos)
+        function insert(L, e, pos)
             front = L.data(1:pos);
             back = L.data(pos+1:L.len);
-            Lnew = sxm_List([front e back]);
+            L.data = [front e back];
+            L.len = L.len + length(e);
         end
         
-        function Lnew = remove(L, pos)
+        function remove(L, pos)
             front = L.data(1:pos-1);
             back = L.data(pos+1:L.len);
-            Lnew = sxm_List([front back]);
+            L.data = [front back];
+            L.len = L.len - 1;
         end
         
         function Lnew = plus(L1, L2)
             Lnew = sxm_List(L1.data);
-            Lnew.append(L2.data);
+            Lnew.extend(L2.data);
         end
         
         function r = in(L, e)
             r = sxm_in(e, L.data);
         end
         
-        function Lnew = sort(L)
-            Lnew = sxm_List(sort(L.data));
+        function sort(L)
+            L.data = sort(L.data);
+        end
+        
+        function extend(L, l)
+            L.append(l);
         end
     end 
 end
